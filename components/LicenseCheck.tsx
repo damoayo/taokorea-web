016939f7f6d4f1ffee910bc7fe1b0b08df8541d1
damoayo <mamapai@naver.com>
2026-03-29 01:35:02 +0900

    확인커밋

--- components/LicenseCheck.tsx
+++ components/LicenseCheck.tsx
@@ -4,8 +4,10 @@ import config from "devextreme/core/config";
 
 const LicenseCheck = () => {
   if (typeof window !== "undefined") {
-    // .env에 저장한 키를 불러옵니다.
     const key = process.env.NEXT_PUBLIC_DEVEXTREME_LICENSE_KEY;
+    
+    // 코드 실행 시점에 브라우저 콘솔에 찍히도록 설정
+    console.log("Vercel 환경변수 로드 상태:", key ? `${key.substring(0, 5)}...` : "키 없음 (undefined)");
     
     if (key) {
       config({ licenseKey: key });
